4ca569e65305653730b470b8c7be1824de435978
AxelMatus <axelantonio@gmail.com>
2022-05-03 00:25:43 -0600

    Add project files.

---
+++ Laboratorio3/Controllers/SongController.cs
@@ -0,0 +1,34 @@
+﻿using Laboratorio3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio3.Controllers
+{
+    public class SongController : Controller
+    {
+        public IActionResult Index()
+        {
+            var songs = GetListOfSongs();
+            ViewBag.MainTitle = "My favorite song";
+            return View(songs);
+        }
+
+        private List<SongModel> GetListOfSongs()
+        {
+            List<SongModel> songs = new List<SongModel>();
+            songs.Add(new SongModel
+            {
+                Id = 1,
+                Name = "Midnight City",
+                Artist = "M83",
+                Album = "Hurry Up, We're Dreaming",
+                Genre = "Alternativa",
+                ReleasedDate = new DateTime(2011, 10, 18)
+            }) ;
+            return songs;
+        }
+    }
+}
---
+++ Laboratorio3/Models/SongModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio3.Models
+{
+    public class SongModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleasedDate { get; set; }
+    }
+}
